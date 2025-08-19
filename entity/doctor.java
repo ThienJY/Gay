bd0ee7a98abeded17b53e857748d507ccfb8c215
ThienJY <thienjy-sm24@student.tarc.edu.my>
2025-08-19 10:08:32 +0800

    Create doctor.java

---
+++ entity/doctor.java
@@ -0,0 +1,22 @@
+package entity;
+
+/**
+ * Doctor entity
+ * Author: Thien (student)
+ */
+public class Doctor {
+    private String id;
+    private String name;
+
+    public Doctor(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return String.format("Doctor{id='%s', name='%s'}", id, name);
+    }
+}
